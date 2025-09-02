c8e896b259921e48942d52a6cb8de10d11c93909
sorubadguy <lucasmartinez33@gmail.com>
2025-09-02 11:20:01 -0300

    001- hola data + version python

---
+++ 001-Hola_data_science.py
@@ -0,0 +1,11 @@
+"""
+1. Hola Data Science
+**Descripción:** Imprime en pantalla "Hola Data Science" y la versión de Python instalada.  
+**Objetivo:** Familiarizarse con la sintaxis básica y el entorno.  
+**Librerías sugeridas:** `sys`  
+**Dificultad:** ⭐
+"""
+
+import sys
+
+print(f"Hola Data Science\nVersion de Python: {sys.version[0:6]}")
